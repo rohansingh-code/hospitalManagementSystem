5193f88bf022dd3356c6b827e5159d794d2e71c2
Rohan <rohansingh23931@gmail.com>
2026-03-26 15:41:53 +0530

    Add Department entity and define relationship with Doctor

--- src/main/java/com/example/springboot/hospitalManagement/Entity/Doctor.java
+++ src/main/java/com/example/springboot/hospitalManagement/Entity/Doctor.java
@@ -3,6 +3,9 @@ package com.example.springboot.hospitalManagement.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
@@ -23,4 +26,7 @@ public class Doctor {
 
     @Column(nullable = false,unique = true,length = 100)
     private String email;
+
+    @ManyToMany(mappedBy = "doctors")
+    private Set<Department> departments = new HashSet<>();
 }
